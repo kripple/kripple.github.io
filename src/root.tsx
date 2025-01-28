a962f1a9f16fd88401fd6c8747a56dbef2938d55
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-01-28 10:39:12 -0500

    remove noscript class in dev mode

--- src/root.tsx
+++ src/root.tsx
@@ -3,3 +3,5 @@ import { createRoot } from 'react-dom/client';
 import { App } from '@/components/App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+
+import.meta.env.DEV && document.documentElement.classList.remove('noscript');
