77c07f1ca8c7d48b7397174ae83f80f5d41a27be
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-17 00:47:00 -0500

    style the theme toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import { Icon } from '@/components/Icon';
 import { SocialIcons } from '@/components/SocialIcons';
+import { ThemeToggle } from '@/components/ThemeToggle';
 import { config } from '@/data/config';
-import { ids, sections } from '@/data/sections';
+import { sections } from '@/data/sections';
 
 import '@/components/header.css';
 
@@ -30,28 +30,7 @@ export function Header() {
       </div>
       <div className="header-icon-set">
         <SocialIcons iconsOnly={true} />
-        <label
-          aria-label="theme toggle"
-          // className="theme-toggle"
-          htmlFor={ids.themeToggle}
-          // id="theme-toggle-label"
-          // TODO:
-          // onKeyDown={onKeyDown}
-          role="button"
-          tabIndex={0}
-        >
-          <div className="icon sun-icon">
-            light
-            <div className="circle icon"></div>
-          </div>
-          <div className="icon moon-icon">
-            dark
-            <div className="circle icon"></div>
-          </div>
-          {/* <Icon icon="sun" />
-          <Icon icon="moon" /> */}
-        </label>
-        {/* <ThemeToggle /> */}
+        <ThemeToggle />
       </div>
     </header>
   );
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,7 +4,7 @@ import { ids } from '@/data/sections';
 import '@/components/theme-toggle.css';
 
 export function ThemeToggle() {
-  const themeToggle = (
+  return (
     <label
       aria-label="theme toggle"
       className="theme-toggle"
@@ -15,32 +15,8 @@ export function ThemeToggle() {
       role="button"
       tabIndex={0}
     >
-      change theme
+      <Icon icon="sun" />
+      <Icon icon="moon" />
     </label>
   );
-  const otherThemeToggle = (
-    <label
-      aria-label="theme toggle"
-      className="theme-toggle"
-      htmlFor={ids.themeToggle}
-      // id="theme-toggle-label"
-      // TODO:
-      // onKeyDown={onKeyDown}
-      role="button"
-      tabIndex={0}
-    >
-      {/* <label className="theme-icon-label">Light</label> */}
-      <div className="theme-icons">
-        <div className="theme-icon-light">
-          <Icon icon="sun" />
-        </div>
-        <div className="theme-icon-dark">
-          <Icon icon="moon" />
-        </div>
-      </div>
-      {/* <label className="theme-icon-label">Dark</label> */}
-    </label>
-  );
-
-  return otherThemeToggle;
 }
