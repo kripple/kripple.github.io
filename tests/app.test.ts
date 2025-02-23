d43267f851f5561a4f95b3b2a2248b7e93b360ea
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-23 00:04:47 -0500

    add todo

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-restricted-imports */
 import { expect, test } from '@playwright/test';
+
+import { screens } from './screens';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -7,4 +10,22 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Kelly Ripple/);
 });
 
-// take full-page screenshots for all standard screens?
+// TODO: break into reasonable sections for screenshots
+screens.map((screen) => {
+  const name = `app-${screen.width}px`;
+  const options = { animations: 'disabled', fullPage: true } as const;
+
+  test.skip(`${name} [dark]`, async ({ page }) => {
+    await page.goto('/');
+    await page.setViewportSize({ width: screen.width, height: screen.height });
+    await expect(page).toHaveScreenshot(`${name}-dark.png`, options);
+  });
+
+  test.skip(`${name} [light]`, async ({ page }) => {
+    await page.goto('/');
+    await page.setViewportSize({ width: screen.width, height: screen.height });
+    await page.getByTestId('theme-toggle').click();
+    await page.mouse.move(0, 0);
+    await expect(page).toHaveScreenshot(`${name}-light.png`, options);
+  });
+});
