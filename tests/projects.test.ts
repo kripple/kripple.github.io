2607e422db7e007cc2a8f32f6ed22c0c83376cbf
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-26 18:39:07 -0500

    add projects section snapshots

--- tests/projects.test.ts
+++ tests/projects.test.ts
@@ -2,30 +2,77 @@
 import { expect, test } from '@playwright/test';
 
 import { screens } from './screens';
-import { projects } from '../src/data/projects';
+// import { projects } from '../src/data/projects';
 
-const screenshots = screens.filter((screen) => screen.primary);
-function capture({ project, url }: { project: string; url: string }) {
-  screenshots.map((screen) => {
-    const name = `${project}-${screen.width}x${screen.height}`;
-    const options = {
-      animations: 'disabled',
-      threshold: 0,
-    } as const;
+// const screenshots = screens.filter((screen) => screen.primary);
+// function capture({ project, url }: { project: string; url: string }) {
+//   screenshots.map((screen) => {
+//     const name = `${project}-${screen.width}x${screen.height}`;
+//     const options = {
+//       animations: 'disabled',
+//       threshold: 0,
+//     } as const;
 
-    test.skip(name, async ({ page }) => {
-      await page.goto(url);
-      await page.setViewportSize({
-        width: screen.width,
-        height: screen.height,
-      });
+//     test.skip(`${name} [dark]`, async ({ page }) => {
+//       await page.goto(url);
+//       await page.setViewportSize({
+//         width: screen.width,
+//         height: screen.height,
+//       });
 
-      await page.waitForTimeout(500);
-      await expect(page).toHaveScreenshot(`${name}.png`, options);
+//       await page.waitForTimeout(500);
+//       await expect(page.locator('.projects-section')).toHaveScreenshot(
+//         `${name}.png`,
+//         options,
+//       );
+//     });
+
+//     test.skip(`${name} [light]`, async ({ page }) => {
+//       await page.goto(url);
+//       await page.setViewportSize({
+//         width: screen.width,
+//         height: screen.height,
+//       });
+//       await page.waitForTimeout(500);
+//       await expect(page.locator('.projects-section')).toHaveScreenshot(
+//         `${name}.png`,
+//         options,
+//       );
+//     });
+//   });
+// }
+
+// projects.map(({ key: project, websiteUrl: url }) => {
+//   capture({ project, url });
+// });
+
+screens.map((screen) => {
+  const name = `projects-${screen.width}px`;
+  const options = {
+    animations: 'disabled',
+    threshold: 0.1,
+  } as const;
+
+  test(`${name} [dark]`, async ({ page }) => {
+    await page.goto('/');
+    await page.setViewportSize({
+      width: screen.width,
+      height: screen.height,
     });
+    await expect(page.locator('.projects-section')).toHaveScreenshot(
+      `${name}-dark.png`,
+      options,
+    );
   });
-}
 
-projects.map(({ key: project, websiteUrl: url }) => {
-  capture({ project, url });
+  test(`${name} [light]`, async ({ page }) => {
+    await page.goto('/');
+    await page.setViewportSize({ width: screen.width, height: screen.height });
+    await page.getByTestId('theme-toggle').click();
+    await page.mouse.move(0, 0);
+    await expect(page.locator('.projects-section')).toHaveScreenshot(
+      `${name}-light.png`,
+      options,
+    );
+  });
 });
