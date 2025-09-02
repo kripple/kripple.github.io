7ec26a9cceab6344dc53d21de0f76f255038562b
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-01 20:40:52 -0400

    update capture script

--- src/helpers/capture.ts
+++ src/helpers/capture.ts
@@ -1,9 +1,14 @@
-/* eslint-disable @typescript-eslint/no-restricted-imports */
+import fs from 'fs';
+
 import { expect, test } from '@playwright/test';
 
+const snapshotsPath = 'src/helpers/snapshots';
 const projects = {
   pokematch: 'https://kripple.github.io/pokematch',
   repos: 'https://kripple.github.io/repos',
+  klinky: 'https://klinky.link',
+  codebreaker: 'https://kripple.github.io/codebreaker',
+  'map-slicer': 'https://kripple.github.io/map-slicer',
 };
 
 const width = 1440;
@@ -12,10 +17,16 @@ const options = { animations: 'disabled' } as const;
 
 Object.entries(projects).forEach(([projectKey, url]) => {
   const file = `${projectKey}.png`;
+
+  // Skip if file already exists
+  if (fs.existsSync(`${snapshotsPath}/${file}`)) {
+    return;
+  }
+
   test(`capture - ${file}`, async ({ page }) => {
     await page.goto(url);
     await page.setViewportSize({ width, height });
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(2000);
     await expect(page).toHaveScreenshot(file, options);
   });
 });
