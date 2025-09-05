f6fe3f79c2c7bd0fd723a9dfa8216015a806eaad
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-04 21:09:34 -0400

    add github action to lint, test, and deploy

---
+++ src/helpers/lighthouse.ts
@@ -0,0 +1,39 @@
+import lighthouse from 'lighthouse';
+import puppeteer from 'puppeteer';
+
+const url = 'https://kripple.github.io' as const;
+const port = 9222 as const;
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    '--disable-dev-shm-usage',
+    '--disable-extensions',
+    '--disable-gpu',
+    '--disable-infobars',
+    '--disable-setuid-sandbox',
+    '--no-sandbox',
+    '--no-zygote',
+    `--remote-debugging-port=${port}`,
+  ],
+});
+const result = await lighthouse(url, { port });
+if (!result) throw Error('failed to create lighthouse report');
+
+for (const category in result.lhr.categories) {
+  const draft = result.lhr.categories[category].score;
+  if (!draft) throw Error(`missing score for '${category}' category`);
+
+  const defaultThreshold = 100 as const;
+  const performanceThreshold = 99 as const;
+
+  const score = draft * 100;
+  if (
+    (category !== 'performance' && score < defaultThreshold) ||
+    (category === 'performance' && score < performanceThreshold)
+  ) {
+    throw Error(`failed ${category} audit with a score of ${score}`);
+  }
+  console.info(`${category}: ${score}`);
+}
+
+await browser.close();
