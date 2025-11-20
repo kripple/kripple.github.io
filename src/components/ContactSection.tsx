4801f0d8ba645bfda20fc6e60d79a269fde80433
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-11-20 15:07:40 -0500

    update contact section content

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -10,10 +10,12 @@ export function ContactSection() {
   return (
     <section className="section contact-section" id={ids.contact}>
       <div>
-        <h3 className="section-title contact-section-title">Contact</h3>
+        <h3 className="section-title contact-section-title show-for-desktop">
+          Contact
+        </h3>
 
         <p className="paragraph">
-          {`I'm actively seeking my next full-time role and available to start immediately. If you know of opportunities that might be a good fit, I'd love to connect!`}
+          {`I'm a full-stack software engineer specializing in frontend development. I'm not seeking new opportunities at this time, but for all other inquiries, please use the contact information below:`}
         </p>
 
         <p className="paragraph add-punctuation">
